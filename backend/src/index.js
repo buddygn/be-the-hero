39135e09a7e82b6d35436224ccb7acbc7ba6c7e9
Geison <buddygn@gmail.com>
2020-03-25 23:55:25 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333);
+
+/** 
+ * Metodos HTTP:
+ * 
+ * GET: Buscar informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+*/
+
+/**
+ * Tipos de parametros
+ * 
+ * Query: Parametros nomeados enviados na rota apos "?" (filtros, paginacao)
+ * Route: Parametros utilizados para identificar recursos /1
+ * Request Body: Corpo da requisicao, utilizado para criar ou alterar recursos
+ */
+
+
